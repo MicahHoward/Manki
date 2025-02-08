d0e5714e1ffe79d2584dfd9bae068e885fb964f1
Micah Howard <mhoward@westmont.edu>
2025-02-08 08:37:35 -0800

    Created skill for two digit addition with solution checking

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include "skills/addition.h"
 using namespace std;
 
 int main()
 {
-        cout << "Hello world!";
-        return 0;
+        int* additionProblem = generate_problem();
+
+        int firstNumber = additionProblem[0];
+        int secondNumber = additionProblem[1];
+        int sum = additionProblem[2];
+        
+        int userGuess;
+
+        cout << firstNumber << " + " << secondNumber << " = ?" << endl;
+        cin >> userGuess;
+
+        if(userGuess == sum){
+                cout << "Correct!" << endl;
+        } else{
+                cout << "Incorrect!" << endl;
+        }
+
+        cout << "You guessed " << userGuess << " and the answer was " << sum << "." << endl;
+
+       return 0;
 }
---
+++ src/skills/addition.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+int* generate_problem() 
+{
+        int min = 1;
+        int max = 100;
+
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> distrib(min, max);
+
+        int first_number = distrib(gen);
+        int second_number = distrib(gen);
+        int sum = first_number + second_number;
+
+        int* returnValues = new int[3];
+        returnValues[0] = first_number;
+        returnValues[1] = second_number;
+        returnValues[2] = sum;
+
+        return returnValues;
+}
