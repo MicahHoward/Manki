a70dbaeeee952cf62cbc6d51abdf74a798a9c27e
Micah Howard <mhoward@westmont.edu>
2025-02-08 23:11:41 -0800

    Created initial FSRS implentation, does not update retrievability with time yet

--- src/db.h
+++ src/db.h
@@ -1,6 +1,9 @@
 #pragma once
+using namespace std;
 static int callback();
 int initialize_database();
 int insert_default_values();
 int read_database();
-
+float get_skill_value(int skillID, string attribute);
+int update_skill_value(int skillID, string attribute, float newValue);
+int update_skill_value(int skillID, string attribute, string newValue);
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,84 @@
 #include <iostream>
+#include <chrono>
 #include "skills/addition.h"
 #include "skills/subtraction.h"
 #include "skills/multiplication.h"
 #include "db.h"
+#include "fsrs.h"
 using namespace std;
+
+int update_fsrs_on_answer(int skillID, int grade)
+{
+        auto now = chrono::system_clock::now();
+        time_t reviewTime = chrono::system_clock::to_time_t(now);
+
+        float currentStability = get_skill_value(skillID, "STABILITY");
+        float currentDifficulty = get_skill_value(skillID, "DIFFICULTY");
+        float currentRetrievability = get_skill_value(skillID, "RETRIEVABILITY");
+        
+        if(currentStability == -1){
+                initialize_stability(skillID, grade);
+                currentStability = get_skill_value(skillID, "STABILITY");
+        } else if(grade == 3){
+                update_stability_after_recall(skillID, grade, currentDifficulty, currentStability, currentRetrievability);
+        } else if(grade == 1){
+                update_stability_after_lapse(skillID, currentDifficulty, currentStability, currentRetrievability);
+        } else{
+                cout << "Invalid grade inputted to update_fsrs_on_answer" << endl;
+        }
+
+        if(currentDifficulty == -1){
+                initialize_difficulty(skillID, grade);
+                currentDifficulty = get_skill_value(skillID, "DIFFICULTY");
+        } else{
+                update_difficulty(skillID, currentDifficulty, grade);
+        }
+
+        update_last_review_time(skillID, to_string(reviewTime));
+
+        // time = 0, since update_fsrs is only called on review
+        update_retrievability(skillID, 0, currentStability);
+        return 0;
+}
+
+int update_fsrs_on_time()
+{
+        return 0;
+}
+
+int print_skill_info(int skillID){
+        cout << endl << "print_skill_info section" << endl;
+        float currentStability = get_skill_value(skillID, "STABILITY");
+        float currentDifficulty = get_skill_value(skillID, "DIFFICULTY");
+        float currentRetrievability = get_skill_value(skillID, "RETRIEVABILITY");
+        float currentLastReviewTime = get_skill_value(skillID, "LAST_REVIEW_TIME");
+        cout << "For skillID " << to_string(skillID) << ":" << endl; 
+        cout << "Current stability = " << to_string(currentStability) << endl;
+        cout << "Current difficulty = " << to_string(currentDifficulty) << endl;
+        cout << "Current retrievability = " << to_string(currentRetrievability) << endl;
+        cout << "Current last review time = " << to_string(currentLastReviewTime) << endl;
+        return 0;
+}
 
 int main()
 {
         initialize_database();
         insert_default_values();
         read_database();
-        int problemType;
+        int skillID;
         int* problem;
         string operand;
         cout << "What type of problem would you like?" << endl;
-        cout << "Please enter 1 for addition, 2 for subtraction, or three for multiplication." << endl;
-        cin >> problemType;
+        cout << "Please enter 1 for addition, 2 for subtraction, or 3 for multiplication." << endl;
+        cin >> skillID;
 
-        if(problemType == 1){
+        if(skillID == 1){
                 problem = generate_addition_problem();
                 operand = "+";
-        } else if(problemType == 2){
+        } else if(skillID == 2){
                 problem = generate_subtraction_problem();
                 operand = "-";
-        } else if(problemType == 3){
+        } else if(skillID == 3){
                 problem = generate_multiplication_problem();
                 operand = "x";
         } else{
@@ -32,22 +87,27 @@ int main()
 
         int firstNumber = problem[0];
         int secondNumber = problem[1];
-        int sum = problem[2];
+        int solution = problem[2];
         
         int userGuess;
 
         cout << firstNumber << " " << operand << " " << secondNumber << " = ?" << endl;
         cin >> userGuess;
 
-        if(userGuess == sum){
+        if(userGuess == solution){
                 cout << "Correct!" << endl;
+                update_fsrs_on_answer(skillID, 3);
         } else{
                 cout << "Incorrect!" << endl;
+                update_fsrs_on_answer(skillID, 1);
         }
 
-        cout << "You guessed " << userGuess << " and the answer was " << sum << "." << endl;
+        cout << "You guessed " << userGuess << " and the answer was " << solution << "." << endl;
 
-       return 0;
+        print_skill_info(skillID);
+
+
+
+        return 0;
 }
-
 
