ea8cc5cb315fd9c22c13a90eb31d88366deb606b
Micah Howard <mhoward@westmont.edu>
2025-03-23 13:44:30 -0700

    Fixed program title, added title to stats message box

--- src/db.h
+++ src/db.h
@@ -5,6 +5,7 @@ int insert_default_values();
 int read_database();
 float get_skill_value(int skillID, std::string attribute);
 int get_number_of_skills();
+std::string get_skill_name(int skillID);
 std::string* get_skill_names();
 int update_skill_value(int skillID, std::string attribute, float newValue);
 int update_skill_value(int skillID, std::string attribute, std::string newValue);
